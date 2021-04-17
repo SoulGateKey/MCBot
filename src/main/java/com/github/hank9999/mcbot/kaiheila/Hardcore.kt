b1b476d01dbfc63316e69c2250561da2e9039127
hank9999 <hank9999@qq.com>
2021-04-17 20:17:44 +0800

    change normal class of hardcore to sealed class

--- src/main/java/com/github/hank9999/mcbot/kaiheila/Hardcore.kt
+++ src/main/java/com/github/hank9999/mcbot/kaiheila/Hardcore.kt
@@ -1,9 +1,11 @@
 package com.github.hank9999.mcbot.kaiheila
 
-class Hardcore {
-    object Api {
-        object Websocket {
-            const val gatewayUrl: String = "https://www.kaiheila.cn/api/v3/gateway/index?compress=0"
+sealed class Hardcore {
+    sealed class Api {
+        sealed class Websocket {
+            companion object {
+                const val gatewayUrl: String = "https://www.kaiheila.cn/api/v3/gateway/index?compress=0"
+            }
         }
     }
 }
